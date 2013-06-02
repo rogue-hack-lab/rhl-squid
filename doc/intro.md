e94a7e05c4428e4a099867093360783e3d0ff832
J. David Lowe <j.david.lowe@gmail.com>
2013-06-02 08:30:41 -0700

    lein new

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to rhl-squid
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/rhl_squid/core_test.clj
@@ -0,0 +1,7 @@
+(ns rhl-squid.core-test
+  (:use clojure.test
+        rhl-squid.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
