0f631e5942aa0b4f53f9abfc5be4e19d731bd399
J. David Lowe <j.david.lowe@gmail.com>
2013-06-02 10:04:06 -0700

    read meetup events

--- project.clj
+++ project.clj
@@ -3,5 +3,9 @@
   :url "https://github.com/rogue-hack-lab/rhl-squid"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.incubator "0.1.2"]
+                 [org.clojure/tools.reader "0.7.4"]
+                 [slingshot "0.10.3"]
+                 [clj-http "0.7.2"]]
   :main rhl-squid.core)
--- src/rhl_squid/core.clj
+++ src/rhl_squid/core.clj
@@ -1,7 +1,26 @@
 (ns rhl-squid.core
-  (:gen-class))
+  (:require [clojure.core.strint :refer (<<)]
+            [clojure.pprint :refer (pprint)]
+            [clojure.tools.reader.edn :as edn]
+            [slingshot.slingshot :refer (try+)]
+            [rhl-squid.meetup :as meetup]))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (if-let [config-path (first args)]
+    (do
+      (let [config (edn/read-string (slurp config-path))]
+        (try+
+          (meetup/validate-config config)
+          (catch :config-error {error :config-error}
+            (println (<< "Error: ~{error}"))
+            (System/exit 1)))
+        (try+
+          (meetup/validate-environment)
+          (catch :env-error {error :env-error}
+            (println (<< "Error: ~{error}"))
+            (System/exit 1)))
+        (pprint (meetup/get-events config))))
+    (do
+      (println "Usage: lein run <squid.edn>")
+      (System/exit 1))))
